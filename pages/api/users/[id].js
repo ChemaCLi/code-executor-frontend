d30cd592a2e3eca65220c7cb84a59ea6e80a5dc4
Jose Maria CLi <chema9703@gmail.com>
2022-08-28 23:46:56 -0500

    Add users CRUD functions

---
+++ pages/api/users/[id].js
@@ -0,0 +1,51 @@
+import { prisma } from "../prisma-client"
+
+export default async function handleer(req, res) {
+  try {
+    switch (req.method) {
+      case 'PATCH':
+        return updateUser(req, res)
+      case 'DELETE':
+        return deleteUser(req, res)
+      default:
+        return res.status(405).json({ error: "Method not allowed in this path" })
+    }
+  } catch (e) {
+    res.status(500).json({ error: e?.message || e })
+  } finally {
+    prisma.$disconnect()
+  }
+}
+
+const updateUser = async (req, res) => {
+  const {
+    body: { email, name },
+    query: { id }
+  } = req
+
+  const updatedUser = await prisma.user.update({
+    where: { id: Number(id) },
+    data: {
+      name,
+      email,
+    }
+  })
+  res.status(202).json({ data: updatedUser })
+  return updatedUser
+}
+
+
+const deleteUser = async (req, res) => {
+  const {
+    query: { id },
+  } = req
+
+  const userId = Number(id)
+
+  const deletedUser = await prisma.user.delete({
+    where: { id: userId }
+  })
+
+  res.status(202).json({ data: deletedUser })
+  return deletedUser
+}
--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,25 +1,26 @@
 import { prisma } from "../prisma-client"
 
 export default async function handleer(req, res) {
-  if (req.method === 'POST') {
-    createUser(req, res)
-  }
-
-  if (req.method === 'GET') {
-    getUsers(req, res)
-  }
-}
-
-const getUsers = async (req, res) => {
   try {
-    const users = await prisma.user.findMany()
-    res.status(200).json({ data: users })
-    return users
+    switch (req.method) {
+      case 'POST':
+        return createUser(req, res)
+      case 'GET':
+        return getUsers(req, res)
+      default:
+        return res.status(405).json({ error: "Method not allowed in this path" })
+    }
   } catch (e) {
     res.status(500).json({ error: e?.message || e })
   } finally {
     prisma.$disconnect()
   }
+}
+
+const getUsers = async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.status(200).json({ data: users })
+  return users
 }
 
 const createUser = async (req, res) => {
@@ -27,20 +28,12 @@ const createUser = async (req, res) => {
     body: { email, name },
   } = req
 
-  try {
-    const newUser = await prisma.user.create({
-      data: {
-        name,
-        email,
-      }
-    })
-    res.status(201).json({ data: newUser })
-    return newUser
-  } catch (e) {
-    res.status(500).json({ error: e?.message || e })
-  } finally {
-    prisma.$disconnect()
-  }
+  const newUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+    }
+  })
+  res.status(201).json({ data: newUser })
+  return newUser
 }
-
-
